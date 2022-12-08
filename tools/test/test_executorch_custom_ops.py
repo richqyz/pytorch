a506973d3f8b466053f77bc3f24872db2516c284
Larry Liu <8188269+larryliu0820@users.noreply.github.com>
2022-12-08 00:09:59 -0800

    [torchgen] Move Executorch custom ops logic into torchgen
    Differential Revision: [D41597382](https://our.internmc.facebook.com/intern/diff/D41597382/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Meta-specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D41597382/)!
    
    ghstack-source-id: bc544ec6bdfcfd1244e47ee2e72a333ff1a00114
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/90099

---
+++ tools/test/test_executorch_custom_ops.py
@@ -0,0 +1,72 @@
+import unittest
+
+from typing import Dict
+from torchgen.executorch.api.custom_ops import ComputeNativeFunctionStub
+
+from torchgen.model import Location, NativeFunction
+
+SPACES = "    "
+
+
+def _get_native_function_from_yaml(yaml_obj: Dict[str, object]) -> NativeFunction:
+    native_function, _ = NativeFunction.from_yaml(
+        yaml_obj,
+        loc=Location(__file__, 1),
+        valid_tags=set(),
+    )
+    return native_function
+
+
+class TestComputeNativeFunctionStub(unittest.TestCase):
+    """
+    Could use torch.testing._internal.common_utils to reduce boilerplate.
+    GH CI job doesn't build torch before running tools unit tests, hence
+    manually adding these parametrized tests.
+    """
+    def _test_function_schema_generates_correct_kernel(self, obj, expected) -> None:
+        func = _get_native_function_from_yaml(obj)
+
+        gen = ComputeNativeFunctionStub()
+        res = gen(func)
+        self.assertEquals(
+            res,
+            expected,
+        )
+
+    def test_function_schema_generates_correct_kernel_tensor_out(self):
+        obj = {
+            "func": "custom::foo.out(Tensor self, *, Tensor(a!) out) -> Tensor(a!)"
+        }
+        expected = """
+at::Tensor & wrapper_out_foo_out(const at::Tensor & self, at::Tensor & out) {
+    return out;
+}
+    """
+        self._test_function_schema_generates_correct_kernel(obj, expected)
+
+    def test_function_schema_generates_correct_kernel_no_out(self):
+        obj = {"func": "custom::foo.Tensor(Tensor self) -> Tensor"}
+        expected = """
+at::Tensor wrapper_Tensor_foo(const at::Tensor & self) {
+    return self;
+}
+    """
+        self._test_function_schema_generates_correct_kernel(obj, expected)
+
+    def test_function_schema_generates_correct_kernel_no_return(self):
+        obj = {"func": "custom::foo(Tensor self, *, Tensor(a!)[] out) -> ()"}
+        expected = f"""
+void wrapper__foo_out(const at::Tensor & self, at::TensorList out) {{
+{SPACES}
+}}
+    """
+        self._test_function_schema_generates_correct_kernel(obj, expected)
+
+    def test_schema_has_no_return_type_argument_throws(self) -> None:
+        func = _get_native_function_from_yaml(
+            {"func": "custom::foo.bool(Tensor self) -> bool"}
+        )
+
+        gen = ComputeNativeFunctionStub()
+        with self.assertRaisesRegex(Exception, "Can't handle this return type"):
+            gen(func)
